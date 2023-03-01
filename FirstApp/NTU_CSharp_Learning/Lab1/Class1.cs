9501686901648cfc32cdefb055d349d4ebf53596
ricardofrancocustodio <ricardofranco.qa@gmail.com>
2023-03-01 01:00:16 -0300

    Add project files.

---
+++ FirstApp/NTU_CSharp_Learning/Lab1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.NTU_CSharp_Learning.Lab1
+{
+    public class Class1
+    {
+        private int Id;
+        private string Name;
+
+        public Class1(int Idinput, string Nameinput)
+        {
+            Id = Idinput;
+            Name = Nameinput;
+
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public void SetId(int IdNum)
+        {
+            Id = IdNum;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string Namestr)
+        {
+            Name = Namestr;
+        }
+
+
+        public override string ToString()
+        {
+            return "Id=" + Id + ", Name=" + Name ;
+        }
+
+    }
+}
---
+++ FirstApp/NTU_CSharp_Learning/Lab1/TestClass1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.NTU_CSharp_Learning.Lab1
+{
+    internal class TestClass1
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new(1, "João");
+
+            Console.WriteLine(class1);
+        }
+    }
+}
